7446d8d1024e738823b007978d0ae90c3ce45829
1soo <aaa4799@naver.com>
2026-07-24 15:57:16 +0900

    ~ch02: p_04

--- practice/ch02/p_04/Main.java
+++ practice/ch02/p_04/Main.java
@@ -3,40 +3,26 @@ package practice.ch02.p_04;
 import java.util.*;
 
 public class Main {
-    public static String solution(int n, int[] a, int[] b) {
+    public static String solution(int n) {
         StringBuilder sb = new StringBuilder();
+        int first = 1, second = 1;
 
-        for (int i = 0; i < n; i++) {
-            if(a[i] == b[i]) {
-                sb.append("D");
-            }else if(a[i] == 3) {
-                sb.append(b[i] == 1 ? "B" : "A");
-            }else if(b[i] == 3) {
-                sb.append(a[i] == 1 ? "A" : "B");
-            }else {
-                sb.append(a[i] > b[i] ? "A" : "B");
-            }
-            sb.append("\n");
+        sb.append(first).append(" ").append(second);
+
+        for (int i = 3; i <= n; i++) {
+            int tmp = second;
+            second = first + second;
+            first = tmp;
+            sb.append(" ").append(second);
         }
 
         return sb.toString();
     }
 
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
 
-        int[] a = new int[n], b = new int[n];
-        
-        for (int i = 0; i < n; i++) {
-            a[i] = in.nextInt();
-        }
-
-        for (int i = 0; i < n; i++) {
-            b[i] = in.nextInt();
-        }
-
-        System.out.print(solution(n, a, b));
+        System.out.print(solution(n));
     }
 }
