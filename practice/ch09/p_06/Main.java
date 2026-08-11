d3ee565f528b8720ee5e8276b2572c8bbf7a8858
1soo <aaa4799@naver.com>
2026-08-12 08:21:56 +0900

    ~ch09:p_07

---
+++ practice/ch09/p_06/Main.java
@@ -0,0 +1,38 @@
+package practice.ch09.p_06;
+
+import java.util.*;
+
+public class Main {
+    static int[] unf;
+
+    public static int find(int v) {
+        if(v == unf[v]) return v;
+        else return unf[v] = find(unf[v]);
+    }
+
+    public static void union(int a, int b) {
+        int ra = find(a);
+        int rb = find(b);
+        if(ra != rb) unf[ra] = rb;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        unf = new int[n + 1];
+        for(int i = 1; i <= n; i++) {
+            unf[i] = i;
+        }
+        
+        for(int i = 0; i < m; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            union(a, b);
+        }
+
+        int v1 = in.nextInt();
+        int v2 = in.nextInt();
+        System.out.println(find(v1) == find(v2) ? "YES" : "NO");
+    }
+}
