30e23fee329cd03c3f1661a2ef154aef8db0270b
1soo <aaa4799@naver.com>
2026-08-12 21:04:56 +0900

    ch09:0_07

--- practice/ch09/p_07/Main.java
+++ practice/ch09/p_07/Main.java
@@ -3,36 +3,42 @@ package practice.ch09.p_07;
 import java.util.*;
 
 public class Main {
-    static int[] unf;
+    static class Edge implements Comparable<Edge> {
+        int node, dist;
 
-    public static int find(int v) {
-        if(v == unf[v]) return v;
-        else return unf[v] = find(unf[v]);
-    }
+        Edge(int node, int dist) {
+            this.node = node;
+            this.dist = dist;
+        }
 
-    public static void union(int a, int b) {
-        int ra = find(a);
-        int rb = find(b);
-        if(ra != rb) unf[ra] = rb;
+        @Override
+        public int compareTo(Edge o) {
+            return this.dist - o.dist;
+        }
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int m = in.nextInt();
-        unf = new int[n + 1];
-        for(int i = 1; i <= n; i++) {
-            unf[i] = i;
-        }
-        
-        for(int i = 0; i < m; i++) {
-            int a = in.nextInt();
-            int b = in.nextInt();
-            union(a, b);
+
+        List<Queue<Edge>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new PriorityQueue<>());
         }
 
-        int v1 = in.nextInt();
-        int v2 = in.nextInt();
-        System.out.println(find(v1) == find(v2) ? "YES" : "NO");
+        for (int i = 0; i < m; i++) {
+            int start = in.nextInt();
+            int node = in.nextInt();
+            int dist = in.nextInt();
+            graph.get(start).offer(new Edge(node, dist));
+        }
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!graph.get(i).isEmpty())
+                sum += graph.get(i).poll().dist;
+        }
+        System.out.println(sum);
     }
 }
