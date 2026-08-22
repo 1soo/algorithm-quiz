350cc5e8c544951222679002ccbb37ee3c426407
1soo <aaa4799@naver.com>
2026-08-22 10:38:05 +0900

    ~ch10:p_05

--- practice/ch10/p_05/Main.java
+++ practice/ch10/p_05/Main.java
@@ -14,20 +14,18 @@ public class Main {
 
         int m = in.nextInt();
         int[] dy = new int[m + 1];
+        for(int i = 1; i <= m; i++) {
+            dy[i]  = m + 1;
+        }
 
         for(int i = 0; i < n; i++) {
             dy[arr[i]] = 1;
             for(int j = arr[i] + 1; j <= m; j++) {
-                
+                int val = dy[j - arr[i]] + 1;
+                if(val < dy[j]) dy[j] = val;
             }
         }
 
+        System.out.println(dy[m]);
     }
 }
-
-/*
-0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
-0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
-0 0 1 2 2 3 3 4 4 5 5  6  6  7  7  8
-0 0 0 0 0 1 2 2 3 3 2  3  3  4  4  3
-*/
